ca40cc89f321a3623b2ff772820c3e4ac861d756
LeBaudry <146630683+LeBaudry@users.noreply.github.com>
2024-09-18 11:21:11 +0200

    COMMIT GO

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -40,4 +40,26 @@ public class Player : MonoBehaviour
         character.Move(direction * Time.deltaTime);
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            GameManager.Instance.GameOver();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
